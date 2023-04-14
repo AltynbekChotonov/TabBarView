609ba326831be1c61cfd62f14cc82bbf711ed07e
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-04-14 17:29:22 +0600

    iconka TabBarView jasaldy

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+                appBar: AppBar(
+                    title: Text("Flutter Playground"),
+                    bottom: TabBar(tabs: [
+                      Tab(icon: Icon(Icons.announcement)),
+                      Tab(icon: Icon(Icons.cake)),
+                      Tab(icon: Icon(Icons.cloud))
+                    ])),
+                body: TabBarView(children: [
+                  Center(child: Text("Jarıyalar")),
+                  Center(child: Text("Tuulgan kundor")),
+                  Center(child: Text("Data"))
+                ]))));
+  }
+}
